773a9f2d390e3c1698d9b20c6ff782afa6223d08
qcoderx <116906269+qcoderx@users.noreply.github.com>
2025-05-23 07:34:00 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css'; // We'll assume you create this CSS file
 import Menu from './Menu';
-import logo from './assets/journalAIse_logo.png'; 
+import './assets/journalAIse_logo.png'; 
 
 const Header = () => {
   return (
