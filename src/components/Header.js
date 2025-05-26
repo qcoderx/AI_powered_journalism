fe842c1d0d256847eda9c331278c483276574913
qcoderx <116906269+qcoderx@users.noreply.github.com>
2025-05-26 19:41:28 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.css'; // We'll assume you create this CSS file
 import Menu from './Menu';
+import Logo from './JournalAIse_logo.png'
 
 const Header = () => {
   return (
     <header className="app-header">
       <div className="logo-container">
-        <img src='./JournalAIse_logo.png' alt="JournalAIse Logo" className="logo-img" />
+        <img src={Logo} alt="JournalAIse Logo" className="logo-img" />
         <h1 className="logo-text">JournalAISE</h1>
         <p className="tagline">WHERE JOURNALISM MEETS INTELLIGENCE</p>
       </div>
