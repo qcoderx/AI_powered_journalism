f3535032fd88140c02721c6b5e85b8cc8501fda3
qcoderx <116906269+qcoderx@users.noreply.github.com>
2025-05-31 21:51:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // Global styles for your app
-import App from './App'; // Your main App component
+import './index.css'; 
+import App from './App'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
